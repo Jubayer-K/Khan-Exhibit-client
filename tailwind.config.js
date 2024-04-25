6e6b48c69e4108f77592b629144431a96bfc59b4
Jubayer-K <jubayerkhan21@gmail.com>
2024-04-26 04:10:29 +0600

    added github and google providers

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        poppins : "'Poppins', sans-serif",
+        nunito : "'Nunito Sans', sans-serif",
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
